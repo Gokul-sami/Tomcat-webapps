07a6a2f6d561e1a31844f8960598a9b77782bf92
gokul <gokulpandiyan6@gmail.com>
2024-10-01 23:16:18 +0530

    new changes

--- WEB-INF/classes/UserDetailsServlet.java
+++ WEB-INF/classes/UserDetailsServlet.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.net.URLEncoder;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,31 +14,27 @@ public class UserDetailsServlet extends HttpServlet {
         String title = "Form Inputs";
         String name = request.getParameter("userName");
         String email = request.getParameter("userEmail");
+        String phone = request.getParameter("userPhno");
         String age = request.getParameter("userAge");
-        String artist = request.getParameter("favoriteArtist");
-        String genre = request.getParameter("userGenre");
-        String comments = request.getParameter("userComments"); // Add this if you have a comments field
-        System.out.println("running");
+        String address = request.getParameter("userAddress");
+        String dob = request.getParameter("userDob");
+        String gender = request.getParameter("userGender");
+        
+        System.out.println("Servlet is running");
 
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
+        // Generate the HTML response
         out.println("<html><head><title>" + title + "</title></head>");
         out.println("<body><h2>Form Submission Details</h2>");
         out.println("<p>Name: " + name + "</p>");
         out.println("<p>Email: " + email + "</p>");
+        out.println("<p>Phone: " + phone + "</p>");
         out.println("<p>Age: " + age + "</p>");
-        out.println("<p>Favorite Artist: " + artist + "</p>");
-        out.println("<p>Favorite Genre: " + genre + "</p>");
-        out.println("<p>Comments: " + comments + "</p>");
+        out.println("<p>Address: " + address + "</p>");
+        out.println("<p>Date of Birth: " + dob + "</p>");
+        out.println("<p>Gender: " + gender + "</p>");
         out.println("</body></html>");
-
-        // Redirect to result.html with query parameters
-        // response.sendRedirect("result.html?name=" + URLEncoder.encode(name, "UTF-8") +
-        //         "&email=" + URLEncoder.encode(email, "UTF-8") +
-        //         "&age=" + URLEncoder.encode(age, "UTF-8") +
-        //         "&artist=" + URLEncoder.encode(artist, "UTF-8") +
-        //         "&genre=" + URLEncoder.encode(genre, "UTF-8") +
-        //         "&comments=" + URLEncoder.encode(comments, "UTF-8"));
     }
 }
